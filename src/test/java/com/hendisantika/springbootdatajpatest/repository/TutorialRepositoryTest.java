9d83be8526011e7e39e06334868aeae61b7022e2
Hendi Santika <hendisantika@gmail.com>
2021-06-26 14:45:35 +0700

    #11 - Add should_find_tutorials_by_title_containing_string Test Case in TutorialRepositoryTest

--- src/test/java/com/hendisantika/springbootdatajpatest/repository/TutorialRepositoryTest.java
+++ src/test/java/com/hendisantika/springbootdatajpatest/repository/TutorialRepositoryTest.java
@@ -83,5 +83,22 @@ class TutorialRepositoryTest {
 
         assertThat(tutorials).hasSize(2).contains(tut1, tut3);
     }
+
+    @Test
+    public void should_find_tutorials_by_title_containing_string() {
+        Tutorial tut1 = new Tutorial("Spring Boot Tut#1", "Desc#1", true);
+        entityManager.persist(tut1);
+
+        Tutorial tut2 = new Tutorial("Java Tut#2", "Desc#2", false);
+        entityManager.persist(tut2);
+
+        Tutorial tut3 = new Tutorial("Spring Data JPA Tut#3", "Desc#3", true);
+        entityManager.persist(tut3);
+
+        Iterable<Tutorial> tutorials = repository.findByTitleContaining("ring");
+
+        assertThat(tutorials).hasSize(2).contains(tut1, tut3);
+    }
+
 }
 
